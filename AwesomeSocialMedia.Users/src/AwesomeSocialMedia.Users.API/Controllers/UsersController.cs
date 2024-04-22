268dc1650b2fd7aec0e4282116271460cbc9a07c
Rafael Coutinho <coutinhor@knowfully.com>
2024-04-22 09:21:53 -0300

    added initial structure for User Api

---
+++ AwesomeSocialMedia.Users/src/AwesomeSocialMedia.Users.API/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeSocialMedia.Users.API.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : ControllerBase
+    {
+        
+    }
+}
---
+++ AwesomeSocialMedia.Users/src/AwesomeSocialMedia.Users.Infrastructure/Persistence/UsersDbContext.cs
@@ -0,0 +1,9 @@
+﻿using AwesomeSocialMedia.Users.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AwesomeSocialMedia.Users.Infrastructure.Persistence
+{
+    public class UsersDbContext {
+        
+    }
+}
